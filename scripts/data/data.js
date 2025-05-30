68806f402838930ad61294d4ef78fe2f5246e301
basboyukhabip <habibasb@student.arteveldehs.be>
2025-05-30 18:32:02 +0200

    correct imagepaths

--- scripts/data/data.js
+++ scripts/data/data.js
@@ -3,16 +3,16 @@
         id: 1,
         title: "Griffin Hinwiset",
         description: "A personal portfolio website for a friend of mine, showcasing his work and skills.",
-        banner: "../../assets/projects/griffin/griffin.png",
+        banner: "assets/projects/griffin/griffin.png",
         details: {
             intro: "This project is a personal portfolio website designed for Griffin Hinwiset, a friend of mine. The website serves as a platform to showcase his work, skills, and achievements in a visually appealing manner.",
 
             codeImages: [
-                "../../assets/projects/griffin/griffin.png",
-                "../../assets/projects/griffin/carreergriffin.png",
-                "../../assets/projects/griffin/griffinabout.png",
-                "../../assets/projects/griffin/code1griffin.png",
-                "../../assets/projects/griffin/code2griffin.png"
+                "assets/projects/griffin/griffin.png",
+                "assets/projects/griffin/carreergriffin.png",
+                "assets/projects/griffin/griffinabout.png",
+                "assets/projects/griffin/code1griffin.png",
+                "assets/projects/griffin/code2griffin.png"
             ],
 
             technologies: ["HTML", "CSS", "JavaScript"],
@@ -25,17 +25,17 @@
         id: 2,
         title: "Gamescom",
         description: "This was my first ever project that i made on school where i was very proud of.",
-        banner: "../../assets/projects/gamescom/gamescom.png",
+        banner: "assets/projects/gamescom/gamescom.png",
         details: {
             intro: "This was my first ever project that i made for school. I was so proud of myself for this, crazy to see how much i developed my skills. This project is a tribute to the Gamescom event, showcasing various elements related to the gaming industry. It includes a detailed layout and interactive features that highlight the excitement of the event.",
 
                         codeImages: [
-                "../../assets/projects/gamescom/griffin.png",
-                "../../assets/projects/gamescom/gamescom.png",
-                "../../assets/projects/gamescom/detailgamescom.png",
-                "../../assets/projects/gamescom/gamescomcode1.png",
-                "../../assets/projects/gamescom/gamescomcode2.png",
-                "../../assets/projects/gamescom/gamescomcode3.png"
+                "assets/projects/gamescom/griffin.png",
+                "assets/projects/gamescom/gamescom.png",
+                "assets/projects/gamescom/detailgamescom.png",
+                "assets/projects/gamescom/gamescomcode1.png",
+                "assets/projects/gamescom/gamescomcode2.png",
+                "assets/projects/gamescom/gamescomcode3.png"
             ],
 
             technologies: ["HTML", "CSS", "JavaScript"],
@@ -45,16 +45,16 @@
         id: 3,
         title: "Weather App",
         description: "A simple weather application that provides current weather information for any location.",
-        banner: "../../assets/projects/todo.png",
+        banner: "assets/projects/todo.png",
         details: {
             intro: "This project is a simple weather application that allows users to get current weather information for any location. It uses a weather API to fetch real-time data and display it in a user-friendly interface.",
 
                         codeImages: [
-                "../../assets/projects/griffin/griffin.png",
-                "../../assets/projects/griffin/carreergriffin.png",
-                "../../assets/projects/griffin/griffinabout.png",
-                "../../assets/projects/griffin/code1griffin.png",
-                "../../assets/projects/griffin/code2griffin.png"
+                "assets/projects/griffin/griffin.png",
+                "assets/projects/griffin/carreergriffin.png",
+                "assets/projects/griffin/griffinabout.png",
+                "assets/projects/griffin/code1griffin.png",
+                "assets/projects/griffin/code2griffin.png"
             ],
 
             technologies: ["HTML", "CSS", "JavaScript"],
